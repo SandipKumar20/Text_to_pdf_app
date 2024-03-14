93ed1fe1b887a5f3d8aa6e42b1e772ba8bf435aa
Sandip <samyorangy@gmail.com>
2024-03-14 05:57:42 +0530

    Add content to the pdf file

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 for filepath in filepaths:
     pdf.add_page()
     filename = Path(filepath).stem
-    name = filename.capitalize()
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=name.capitalize())
+    pdf.cell(w=50, h=8, txt=filename.capitalize(), ln=1)
+
+    # Extracting the content of the text file
+    with open(filepath, "r") as file:
+        content = file.read()
+    # Add the text file content to the pdf file
+    pdf.set_font(family="Times",size=13)
+    pdf.multi_cell(w=0, h=6, txt=content)
 
 pdf.output("pdf_file/output.pdf")
 
